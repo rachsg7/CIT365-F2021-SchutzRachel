78e4aaa5920b93be0545e823a07ce14df1765a1a
rachsg7 <sch19051@byui.edu>
2021-11-26 15:37:14 -0800

    Books API

---
+++ RocinanteUniversity/RocinanteUniversity/Data/RocinanteUniversityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RocinanteUniversity.Models;
+
+namespace RocinanteUniversity.Data
+{
+    public class RocinanteUniversityContext : DbContext
+    {
+        public RocinanteUniversityContext (DbContextOptions<RocinanteUniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RocinanteUniversity.Models.Student> Student { get; set; }
+    }
+}
